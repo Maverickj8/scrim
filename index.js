19e7240ee353ace50bb4b36130c67021e747e5c5
Juliet <albertmaverick99@gmail.com>
2024-02-06 00:40:35 +0100

    updated counter

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let counter = document.querySelector('#counter')
 let btn = document.querySelector('#increment')
+let save =document.querySelector('#save-btn')
+let prevCount = document.querySelector('#prev')
+
 let count = 0
 
 
 function newCount() {
-    count = count + 1
-    counter.innerText =count
+    count += 1
+    counter.innerText = count
+}
+
+function saveCount() {
+    let previousSave = count + " - "
+    prevCount.textContent += previousSave
+    counter.innerText = 0
 }
 btn.addEventListener('click', newCount)
+save.addEventListener('click', saveCount)
